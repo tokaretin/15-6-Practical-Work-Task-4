b303f668cf21b36542312f4da650edebf57141f5
tokaretin <tokaretin@gmail.com>
2023-08-22 17:14:31 +0200

    second test

--- 15-6-Practical-Work-Task-4/15-6-Practical-Work-Task-4.cpp
+++ 15-6-Practical-Work-Task-4/15-6-Practical-Work-Task-4.cpp
@@ -12,37 +12,71 @@
 Советы и рекомендации
 Положительные числа уже отсортированы в порядке возрастания модулей.*/
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
 
-bool compareByAbsoluteValue(int a, int b) { // Функция сравнения по абсолютным значениям.
-    return std::abs(a) < std::abs(b);
-}
+// первое решение
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//bool compareByAbsoluteValue(int a, int b) { // Функция сравнения по абсолютным значениям.
+//    return std::abs(a) < std::abs(b);
+//}
+//
+//int main() {
+//    std::vector<int> numbers = { -100, -50, -5, 1, 10, 15 };
+//
+//    std::sort(numbers.begin(), numbers.end(), compareByAbsoluteValue); // Сортировка вектора по абсолютным значениям.
+//
+//    for (int i = 0; i < numbers.size(); i++)
+//    {
+//        std::cout << numbers[i] << " ";
+//    }
+//
+//     Аналогичная запись
+//    /*for (int num : numbers) 
+//    {
+//        std::cout << num << " ";
+//    }*/
+//
+//    return 0;
+//}
+
+
+
+#include <iostream>
 
 int main() {
-    std::vector<int> numbers = { -100, -50, -5, 1, 10, 15 };
+    int numbers[] = { -100, -50, 5, 6, 60, 70 };
+    int size = sizeof(numbers) / sizeof(numbers[0]);
+    int min = 0;
 
-    std::sort(numbers.begin(), numbers.end(), compareByAbsoluteValue); // Сортировка вектора по абсолютным значениям.
-
-    for (int i = 0; i < numbers.size(); i++)
+    for ( int i = 1; i < size; i++)
     {
-        std::cout << numbers[i] << " ";
+        if (abs(numbers[i]) < abs(numbers[min]))
+        {
+            min = i;
+        }
     }
 
-    // Аналогичная запись
-    /*for (int num : numbers) 
+    int positive = min + 1;
+    int negative = min - 1;
+    
+    if (size > 0)
     {
-        std::cout << num << " ";
-    }*/
-
+        std::cout << numbers[min] << ", ";
+    }
+    while (negative >= 0 || positive < size) {
+        if (negative >= 0 && (abs(numbers[negative]) <= abs(numbers[positive]) || (positive >= size))) {
+            std::cout << numbers[negative] << ", ";
+            negative--;
+        }
+        if (positive < size &&
+            (abs(numbers[positive]) <= abs(numbers[negative]) || (negative < 0))) {
+            std::cout << numbers[positive] << ", ";
+            positive++;
+        }
+    }
     return 0;
 }
-
-    
-
-
-    
- 
       
           
